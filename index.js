a1943a335936875396c53d9debddd2bb367a58b4
William Ryan <wcryan757@gmail.com>
2021-04-02 01:09:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const receiver = [`Ada`, `Brendan`, `Ali`]
+
+const eventVariable = "birthday"
+
+
+function writeCards(receiver, eventVariable) {
+
+    let newArray = [];
+        for (let i = 0; i < receiver.length; i++) {
+            newArray.push(`Thank you, ${receiver[i]}, for the wonderful ${eventVariable} gift!`)
+
+        }
+        debugger;
+
+        return newArray; 
+
+    
+}
+
+console.log(writeCards('receiver', 'eventVariable'));
+
+function countDown(init = 10){
+
+    while (init >= 0){
+        console.log(init --);
+    }
+}
